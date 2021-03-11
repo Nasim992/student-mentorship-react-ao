3a6ebb1a2ad7ed2385d09eac580fdb960e47a78b
milon27 <mdjahidulislammilon@gmail.com>
2021-03-11 13:13:48 +0600

    clone from student react auth branch need to modify now

---
+++ src/utils/helpers/Define.js
@@ -0,0 +1,12 @@
+const Define = {
+    //bootstrap color
+    BT_PRIMARY: "primary",
+    BT_DANGER: "danger",
+    BT_SUCCESS: "success",
+    BT_WARNING: "warning",
+    BT_INFO: "info",
+    //define
+    NOT_SET: "NOT_SET",
+    C_USER: "C_USER",
+}
+export default Define
