3b423dd6129f95fd5370f63f1cecb0c6cbe38ace
milon27 <mdjahidulislammilon@gmail.com>
2021-05-11 23:13:59 +0600

    update react-router to react-router-dom

--- src/components/layouts/ProtectedPage.jsx
+++ src/components/layouts/ProtectedPage.jsx
@@ -1,42 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import CUser from './../../utils/helpers/CUser';
 import axios from 'axios'
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import URL from './../../utils/helpers/URL';
 
 export default function ProtectedPage({ children }) {
     const [authv, setAuthV] = useState(false)
     const [loading, setLoading] = useState(true)
     useEffect(() => {
+        let unmount = false
         const ckLog = async () => {
-            try {
-                const ck = await axios.get('ao/is-loggedin')
-                //give true or false
-                if (!ck.data) {
-                    //so clear the localStorage
-                    console.log("protected, 1,", "ck.data" + ck.data);
-                    CUser.logOut()
-                    setAuthV(false)
-                } else {
-                    if (CUser.getCurrentuser() && CUser.getCurrentuser() !== undefined) {
-                        setAuthV(true)
-                    } else {
+            if (!unmount) {
+                try {
+                    const ck = await axios.get('ao/is-loggedin')
+                    //give true or false
+                    if (!ck.data) {
+                        //so clear the localStorage
+                        console.log("protected, 1,", "ck.data" + ck.data);
                         CUser.logOut()
-                        console.log("protected, 2,", ck.data);
                         setAuthV(false)
+                    } else {
+                        if (CUser.getCurrentuser() && CUser.getCurrentuser() !== undefined) {
+                            setAuthV(true)
+                        } else {
+                            CUser.logOut()
+                            console.log("protected, 2,", ck.data);
+                            setAuthV(false)
+                        }
                     }
-                }
-                setLoading(false)
+                    setLoading(false)
 
-            } catch (e) {
-                //so clear the localStorage
-                CUser.logOut()
-                console.log("protected, 3,", e.message);
-                setAuthV(false)
-                setLoading(false)
+                } catch (e) {
+                    //so clear the localStorage
+                    CUser.logOut()
+                    console.log("protected, 3,", e.message);
+                    setAuthV(false)
+                    setLoading(false)
+                }
             }
         }
         ckLog()
+        return () => {
+            unmount = true
+        }
     }, [loading])
 
 
